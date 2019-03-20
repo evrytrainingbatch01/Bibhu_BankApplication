d17d8b1261afe1a4f33e758ef509c6aa00684f6b
evrytrainingbatch01 <48678186+evrytrainingbatch01@users.noreply.github.com>
2019-03-20 17:41:08 +0530

    Delete EvryBankConfig.java

--- src/com/evry/training/bank/config/EvryBankConfig.java
+++
@@ -1,7 +0,0 @@
-package com.evry.training.bank.config;
-
-public interface EvryBankConfig {
-	public static final String databaseURL="jdbc:mysql://localhost:3306/Evry_Bank";
-	public static final String userId="root";
-	public static final String password="root";
-}
